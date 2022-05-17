356ac45c5853ace78457e7759c0231a80445b383
Noah Taylor <noahntaylor@gmail.com>
2022-05-16 19:56:23 -0700

    Initial Commit
    - Setup React project for text prompt AI

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  const onSubmit = () => {
+    console.log("Form Submit!");
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Text Prompt AI</h1>
+      </header>
+      <main>
+        <form action=""></form>
+      </main>
+    </div>
+  );
+}
+
+export default App;
